f14416911a03baca36e2838275b5b9aa42028be2
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2022-10-17 08:05:26 +0300

    added validations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ApplicationRecord
   has_many :transactions, dependent: :destroy
   belongs_to :user
+  validates :name, presence: true, length: { minimum: 1, maximum: 25 }
+  validates :user_id, presence: true
 end
