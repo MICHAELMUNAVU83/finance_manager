4403c81ae017523484a6e86dd4f0062c78dfa7e0
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2022-10-17 10:26:32 +0300

    corrected linters

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -4,5 +4,4 @@ class Transaction < ApplicationRecord
   validates :name, presence: true, length: { minimum: 1, maximum: 25 }
   validates :amount, presence: true, numericality: { greater_than: 0 }
   validates :user_id, presence: true
-  
 end
